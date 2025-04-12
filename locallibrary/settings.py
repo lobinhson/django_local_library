748630c320abbbca9efc74316b903e23b12e7e95
lobinhson <lobinhson@gmail.com>
2025-04-12 17:02:33 +0700

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hamishwillee.pythonanywhere.com', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://hamishwillee.pythonanywhere.com']
 
 # Application definition
 
